38e30af7ea1803b6a73dc2aef31e65b5e2486853
JOS3brun0 <126594652+JOS3brun0@users.noreply.github.com>
2023-03-04 01:42:01 -0300

    PRUEBA MODULO 3
    Creacion de componentes

--- FRONTEND-NADIA/src/app/app.module.ts
+++ FRONTEND-NADIA/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './COMPONENTES/header/header.component';
+import { BannerComponent } from './COMPONENTES/banner/banner.component';
+import { AcercaDeComponent } from './COMPONENTES/acerca-de/acerca-de.component';
+import { SkillSoftComponent } from './COMPONENTES/skill-soft/skill-soft.component';
+import { EducacionComponent } from './COMPONENTES/educacion/educacion.component';
+import { TrabajoComponent } from './COMPONENTES/trabajo/trabajo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    SkillSoftComponent,
+    EducacionComponent,
+    TrabajoComponent
   ],
   imports: [
     BrowserModule
